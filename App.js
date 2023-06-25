4537a392e9afbe618544ce1cc5c499e0392058de
OleksandrR61 <106624948+OleksandrR61@users.noreply.github.com>
2023-06-25 20:17:40 +0300

    Add
    Added routes

--- App.js
+++ App.js
@@ -1,24 +1,12 @@
 import { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { LogBox, Image } from 'react-native';
+import { LogBox } from 'react-native';
 
 import * as Font from 'expo-font';
 
-import LoginScreen from './Screens/auth/LoginScreen/LoginScreen';
-import RegistrationScreen from './Screens/auth/RegistrationScreen/RegistrationScreen';
-// import PostsScreen from './Screens/main/PostsScreen/PostsScreen';
-// import CreatePostsScreen from './Screens/main/CreatePostsScreen/CreatePostsScreen';
-// import ProfileScreen from './Screens/main/ProfileScreen/ProfileScreen';
-import CommentsScreen from './Screens/main/CommentsScreen/CommentsScreen';
-import HomeScreen from './Screens/main/HomeScreen/HomeScreen';
-import { Container, Header } from './components';
-import userExample from './example/userExample';
-
-const AuthStack = createStackNavigator();
-const MainStack = createStackNavigator();
-// const MainTab = createBottomTabNavigator();
+import MainRoute from './routes/MainRoute/MainRoute';
+import AuthRoute from './routes/AuthRoute/AuthRoute';
+import { Container } from './components';
 
 LogBox.ignoreLogs([
   'Non-serializable values were found in the navigation state',
@@ -26,7 +14,6 @@ LogBox.ignoreLogs([
 
 export default function App() {
   const [ isReady, setIsReady ] = useState(false);
-  const [ user, setUser ] = useState(userExample);
   const [ isAuth, setIsAuth ] = useState(false);
 
   useEffect(() => {
@@ -56,46 +43,8 @@ export default function App() {
 
   return <NavigationContainer>
     {isAuth 
-      ? <MainStack.Navigator initialRouteName='HomeScreen'>
-        <AuthStack.Screen
-          name={'HomeScreen'}
-          component={HomeScreen}
-          options={{
-            headerShown: false,
-          }}
-          initialParams={{
-            handleAuth,
-          }}
-        />
-        <AuthStack.Screen
-          name={'CommentsScreen'}
-          component={CommentsScreen}
-          options={{
-            header: ({navigation}) => <Header title={"Коментарі"} back={navigation.goBack}/>,
-          }}
-        />
-      </MainStack.Navigator>
-      : <AuthStack.Navigator initialRouteName='LoginScreen'>
-        <AuthStack.Screen
-          name={'LoginScreen'}
-          component={LoginScreen}
-          initialParams={{
-            handleSubmit: handleAuth,
-          }}
-          options={{
-            headerShown: false,
-          }}/>
-        <AuthStack.Screen
-          name={'RegistrationScreen'}
-          component={RegistrationScreen}
-          initialParams={{
-            handleSubmit: handleAuth,
-          }}
-          options={{
-            headerShown: false,
-          }}
-        />
-      </AuthStack.Navigator>
+      ? <MainRoute handleAuth={handleAuth} />
+      : <AuthRoute handleAuth={handleAuth} />
     }
   </NavigationContainer>;
 };
